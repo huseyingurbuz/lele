6dc3a3f9ea5b07e06fd97b32b39dbb5d5b4fe97e
Huseyin Gurbuz <hguerbuez@cadenas.de>
2019-09-04 13:24:02 +0200

    'Party'

--- README.md
+++
@@ -1,2 +0,0 @@
-# lele
-test branch
---
+++ test.py
@@ -0,0 +1,11 @@
+print("Hey")
+x = input('Are you readyyyyy?')
+if x == 'y':
+    print('lets go to party :)')
+    print('then drink water')
+else:
+    y = input('you are already drunk?')
+    if y =='y':
+        print('wanna go villa :(')
+        print('with money')
+
