7c49198ee58eb3a51cc6cd0034810557897784fc
Huseyin Gurbuz <hguerbuez@cadenas.de>
2019-09-05 15:58:09 +0200

    Yarisma

--- test2.py
+++ test2.py
@@ -5,3 +5,13 @@ if x == 'y':
 y = input('ye')
 if y == 'y':
     print('ha söyle')
+count =0
+print('Yarisma baslamak üzere')
+while True:
+    count +=1
+    if count <5:
+        print('please wait')
+    elif count ==5:
+        print('haydi Yallah')
+    else:
+        break
