95a81403638adbedcb9d154b13ef7d22a35a935e
Huseyin Gurbuz <hguerbuez@cadenas.de>
2019-09-05 16:10:27 +0200

    Yatirim planlari

--- test.py
+++ test.py
@@ -8,4 +8,10 @@ else:
     if y =='y':
         print('wanna go villa :(')
         print('with money')
-
+######################################### Yeni
+x = input('insaat?')
+y = input('yazilim?')
+if x == 'y':
+    print('iyi para')
+if y == 'y':
+    print('iyi career')
